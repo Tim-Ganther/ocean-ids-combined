141629135d659b3b94bc53c659332a6eb6b145d8
Tim-Ganther <tim@yourcustomcode.com>
2022-02-08 15:50:36 +0100

    hotfix

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import modules.ocean as oc
 import modules.ids as ids
 
-pool_address, did = oc.create_offer()
+token_address, pool_address, did = oc.create_offer()
 
 resourceUUID = ids.provider_main(did_str = did)
 resource_uuid, resource_data = ids.consumer_main()
 
-oc.buy_data(pool_address, resource_data)
+oc.buy_data(token_address, pool_address, resource_data)
